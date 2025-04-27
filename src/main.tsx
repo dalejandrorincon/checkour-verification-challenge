dac277d4911a75eb071831b3c28cbd028d677d22
Diego Rincon <dalejandrorinconp@gmail.com>
2025-04-27 18:42:46 -0500

    chore: add TailwindCSS for styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div>POC Checkout verification</div>
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
   </StrictMode>
 );
